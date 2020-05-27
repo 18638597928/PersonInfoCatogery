c68ff34d0d1f8fc362096ca924548e776c3dd05e
xx <>
2020-05-27 09:56:44 +0800

    update

--- PersonInfoCatogery.podspec
+++ PersonInfoCatogery.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'PersonInfoCatogery'
-  s.version          = '0.2'
+  s.version          = '0.3'
   s.summary          = 'A short description of PersonInfoCatogery.'
 
 # This description is used to generate tags and improve search results.
@@ -28,7 +28,7 @@ TODO: Add long description of the pod here.
   s.source           = { :git => 'https://github.com/18638597928/PersonInfoCatogery.git', :tag => s.version.to_s }
   # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
-  s.ios.deployment_target = '8.0'
+  s.ios.deployment_target = '9.0'
   s.swift_versions = ["4.0", "5.0"]
   s.source_files = 'PersonInfoCatogery/Classes/**/*'
   
--- PersonInfoCatogery/Classes/CTMediator_PersonInfo.swift
+++ PersonInfoCatogery/Classes/CTMediator_PersonInfo.swift
@@ -10,10 +10,14 @@ import CTMediator
 
 public extension CTMediator {
 
-    func personInfoWith(name: String, age: Int) -> UIViewController {
+    func personInfoWith(name: String, age: Int) -> UIViewController? {
         var dict: [String:String] = [:]
         dict["name"] = name
-        dict["agee"] = "\(age)"
-        return self.performTarget("PersonInfoViewController", action: "PersonInfoViewController", params: dict, shouldCacheTarget: false) as! UIViewController
+        dict["age"] = "\(age)"
+        dict[kCTMediatorParamsKeySwiftTargetModuleName] = "PersonInfo"
+        if let viewController = self.performTarget("PersonInfoViewController", action: "PersonInfoViewController", params: dict, shouldCacheTarget: false) as? UIViewController {
+            return viewController
+        }
+        return nil
     }
 }
